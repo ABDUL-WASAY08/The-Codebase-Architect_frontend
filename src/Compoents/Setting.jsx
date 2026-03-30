c4785fc1d6473e6763cfa1931fe71bdc209ee543
ABDUL WASAY <tahawasay1@gmail.com>
2026-03-30 14:06:59 +0500

    fixing seting avatar

--- src/Compoents/Setting.jsx
+++ src/Compoents/Setting.jsx
@@ -12,7 +12,7 @@ const SettingsContent = ({ user,theme,setTheme }) => {
       <h2 className="text-2xl font-semibold text-gray-900 dark:text-white mb-4">Settings</h2>
       <div className="space-y-6">
         <div className='flex flex-col justify-center items-center'>
-          <img className='rounded-full w-[100px] h-auto border-2 border-blue-500' src={user.avatar} alt="User avatar" />
+          <img className='rounded-full w-[100px] h-auto border-2 border-black dark:border-blue-500' src={user.avatar} alt="User avatar" />
           <p className="text-gray-900 dark:text-white font-medium mt-2">{user.name}</p>
           <p className="text-gray-600 dark:text-gray-400">{user.email}</p>
         </div>
