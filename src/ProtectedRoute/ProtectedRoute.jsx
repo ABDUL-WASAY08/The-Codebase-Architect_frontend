288a0006ada91b7da0dfc8533b7d33ab396051e3
ABDUL WASAY <tahawasay1@gmail.com>
2026-04-17 20:07:42 +0500

    fixing console

--- src/ProtectedRoute/ProtectedRoute.jsx
+++ src/ProtectedRoute/ProtectedRoute.jsx
@@ -4,13 +4,9 @@ import useUserStore from '../Store/userStore';
 
 function ProtectedRoute({ children }) {
     const { isAuthenticated, isLoading,user } = useUserStore();
-        console.log(isAuthenticated)
-        console.log(user)
     if (isLoading) {
         return <div>Loading...</div>; 
     }
-
-    // 4. Redirect if NOT authenticated
     if (!isAuthenticated) {
         return <Navigate to="/" replace />;
     }
