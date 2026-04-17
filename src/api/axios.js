843377246c570126b0284754845c8b29cbd561b5
ABDUL WASAY <tahawasay1@gmail.com>
2026-04-17 20:15:38 +0500

    undo changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,14 +4,4 @@ const api=axios.create({
     baseURL:import.meta.env.VITE_BASE_URL,
     withCredentials:true
 });
-
-api.interceptors.response.use(
-    (response)=> response,
-    (error) => {
-    if (error.response?.status === 401) {
-      return Promise.resolve({ data: { success: false, user: null } });
-    }
-    return Promise.reject(error);
-  }
-)
 export default api 
