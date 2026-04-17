f689884f102eb7e6439d005fa850fb59793b2792
ABDUL WASAY <tahawasay1@gmail.com>
2026-04-17 20:14:24 +0500

    fing console issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,15 @@ import axios from "axios"
 const api=axios.create({
     baseURL:import.meta.env.VITE_BASE_URL,
     withCredentials:true
-})
+});
+
+api.interceptors.response.use(
+    (response)=> response,
+    (error) => {
+    if (error.response?.status === 401) {
+      return Promise.resolve({ data: { success: false, user: null } });
+    }
+    return Promise.reject(error);
+  }
+)
 export default api 
